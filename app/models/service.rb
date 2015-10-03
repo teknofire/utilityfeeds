d072e4aab065ace29dceaa7e1156db5168df8666
Will Fisher <will@gina.alaska.edu>
2015-10-02 17:22:04 -0800

    updating to have counts in the menu

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,8 @@ class Service < ActiveRecord::Base
   friendly_id :name, :use => [:slugged]
   validates_presence_of :name, uniqueness: true
 
+  has_many :outage_reports
+
   def should_generate_new_friendly_id?
     !slug? || name_changed?
   end
